f6f53994d770689e31c051744db2b8552f724954
41zu <dominikator.mueka@googlemail.com>
2022-07-05 11:19:14 +0200

    Upgrade to HttpClient5

--- src/main/java/com/lukaspradel/steamapi/webapi/request/SteamWebApiRequestHandler.java
+++ src/main/java/com/lukaspradel/steamapi/webapi/request/SteamWebApiRequestHandler.java
@@ -7,16 +7,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.NameValuePair;
-import org.apache.http.ParseException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.message.BasicNameValuePair;
+import org.apache.hc.core5.net.URIBuilder;
 
 import com.lukaspradel.steamapi.core.SteamApiRequestHandler;
 import com.lukaspradel.steamapi.core.exception.SteamApiException;
@@ -63,14 +63,11 @@ public class SteamWebApiRequestHandler extends SteamApiRequestHandler {
 
 	List<NameValuePair> getRequestParameters(Map<String, String> parametersMap) {
 
-		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+		List<NameValuePair> nvps = new ArrayList<>();
 
 		nvps.add(new BasicNameValuePair("key", getKey()));
 
-		for (Map.Entry<String, String> param : parametersMap.entrySet()) {
-
-			nvps.add(new BasicNameValuePair(param.getKey(), param.getValue()));
-		}
+		parametersMap.entrySet().forEach(param -> nvps.add(new BasicNameValuePair(param.getKey(), param.getValue())));
 
 		return nvps;
 	}
@@ -94,28 +91,25 @@ public class SteamWebApiRequestHandler extends SteamApiRequestHandler {
 		HttpClient client = getHttpClient();
 		HttpGet getRequest = new HttpGet(requestUrl);
 
-		try {
-			HttpResponse response = client.execute(getRequest);
-
-			Integer statusCode = response.getStatusLine().getStatusCode();
+		// this try-with-resources statement closes the connection
+		// no need to close the client (ref https://hc.apache.org/httpcomponents-client-4.5.x/quickstart.html)
+		try (ClassicHttpResponse response = client.execute(null, getRequest)) {
+			Integer statusCode = response.getCode();
 
 			if (!statusCode.toString().startsWith("20")) {
 				if (statusCode.equals(HttpStatus.SC_UNAUTHORIZED)) {
 					throw new SteamApiException(
 							SteamApiException.Cause.FORBIDDEN, statusCode,
-							response.getStatusLine().getReasonPhrase());
+							response.getReasonPhrase());
 				}
 				throw new SteamApiException(SteamApiException.Cause.HTTP_ERROR,
-						statusCode, response.getStatusLine().getReasonPhrase());
+						statusCode, response.getReasonPhrase());
 			}
-
 			return getHttpResponseAsString(response);
-		} catch (IOException e) {
+		} catch (IOException | ParseException e) {
 			throw new SteamApiException(
 					"The Web API request failed due to the following error: "
 							+ e.getMessage(), e);
-		} finally {
-			getRequest.releaseConnection();
 		}
 	}
 
@@ -124,7 +118,7 @@ public class SteamWebApiRequestHandler extends SteamApiRequestHandler {
 		return HttpClientBuilder.create().build();
 	}
 
-	String getHttpResponseAsString(HttpResponse response)
+	String getHttpResponseAsString(ClassicHttpResponse response)
 			throws ParseException, IOException {
 
 		return EntityUtils.toString(response.getEntity());
