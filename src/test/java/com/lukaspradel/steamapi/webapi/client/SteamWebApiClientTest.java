38ed0f196d3a635d494c8f35a3f8bdf3839cab44
MihajloNesic <nesicmihajlo98@gmail.com>
2025-03-02 09:50:59 +0100

    add missing import

--- src/test/java/com/lukaspradel/steamapi/webapi/client/SteamWebApiClientTest.java
+++ src/test/java/com/lukaspradel/steamapi/webapi/client/SteamWebApiClientTest.java
@@ -66,6 +66,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
